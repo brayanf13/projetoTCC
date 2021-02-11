1f23dcfd8f1b51a057fc0d7cc3a5fa6f83b6c9bf
brayanf13 <brayan.amf@gmail.com>
2021-02-11 16:48:52 -0300

    criando conexão com o backend

--- mobile/src/pages/Module/index.js
+++ mobile/src/pages/Module/index.js
@@ -2,32 +2,55 @@ import React, { useState, useEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
 
-// import api from '../../services/api';
+import api from '../../services/api';
 
 import styles from './styles';
 
 export default function Module() {
+    const [modules, setModules] = useState([]);
+    const [loading, setLoading] = useState(false);
     const navigation = useNavigation();
 
     function navigateToInUnity() {
         navigation.navigate('Unidades');
     }
+
+    async function loadModules() {
+        if (loading) {
+            return;
+        }
+
+        if (total > 0 && modules.length === total) {
+            return;
+        }
+
+        setLoading(true);
+
+        const response = await api.get('module');
+
+        setModules(response.data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        loadModules();
+    }, [])
     
     return (
         <View style={styles.container} >
 
             <FlatList
-                data={[1,2,3,4]}
-                style={styles.moduleList}
-                keyExtractor={module => String(module)}
-                // showsVerticalScrollIndicator={true}
-                renderItem={() => (
+                data={modules}
+                style={styles.incidentList}
+                keyExtractor={modu => String(modu.id)}
+                showsVerticalScrollIndicator={false}
+                renderItem={({ item: modu }) => (
                     <View style={styles.module}>
                         <TouchableOpacity
                             style={styles.moduleButton}
                             onPress={navigateToInUnity}
                         >
-                            <Text style={styles.moduleButtonText}>Botão teste</Text>
+                            <Text style={styles.moduleButtonText}>{modu.name}</Text>
                         </TouchableOpacity>
                     </View>
                 )}
