7983be54577503f12c6450d692a43a0a1058768b
Brayan Ferreira <brayan.amf@gmail.com>
2021-05-11 14:38:52 -0300

    corrigindo bug de ip

--- backend/src/controllers/unityController.js
+++ backend/src/controllers/unityController.js
@@ -8,41 +8,49 @@ const { select } = require('../database/connection');
 module.exports = {
 
     async index(request, response) {
-        const unity = await connection('unity').select('*');
+        let unity = await connection('unity').select('*');
 
         if (request.query.search) {
 
             const search = String(request.query.search);
 
-            const filteredModule = search ? unity.filter(unity => unity.name.includes(search)) : unity;
-            // console.log(request.query.search ? 'foi' : 'não foi')
+            let filteredModule = search ? unity.filter(unity => unity.name.includes(search)) : unity;
+            // console.log(request.query.search ? 'foi' : 'n達o foi')
+
+            filteredModule = filteredModule.map(m => ({...m, image_url: 'http://192.168.1.10:3333/uploads/' + m.image_name}))
+
             return response.json(filteredModule);
         } else {
-            // console.log(request.query.search ? 'foi' : 'não foi')
+            // console.log(request.query.search ? 'foi' : 'n達o foi')
+
+            unity = unity.map(m => ({...m, image_url: 'http://192.168.1.10:3333/uploads/' + m.image_name}))
+
             return response.json(unity);
         }
     },
 
     async indexOneById(request, response) {
         const { id } = request.params;
-        const unity = await connection('unity').select('*').where('module_id', id);
+        let unity = await connection('unity').select('*').where('module_id', id);
         
+        unity = unity.map(m => ({...m, image_url: 'http://192.168.1.10:3333/uploads/' + m.image_name}))
+
         if (request.query.search) {
 
             const search = String(request.query.search);
 
             const filteredModule = search ? unity.filter(unity => unity.name.toLowerCase().includes(search.toLowerCase())) : unity;
-            // console.log(request.query.search ? 'foi' : 'não foi')
+            // console.log(request.query.search ? 'foi' : 'n達o foi')
             return response.json(filteredModule);
         } else {
-            // console.log(request.query.search ? 'foi' : 'não foi')
+            // console.log(request.query.search ? 'foi' : 'n達o foi')
             return response.json(unity);
         }
     },
 
     async create(request, response) {
         const { name, content, module_id } = request.body;
-        const image_url = `http://192.168.0.101:3333/uploads/${request.file.filename}`;
+        const image_url = `http://192.168.1.10:3333/uploads/${request.file.filename}`;
         const image_name = request.file.filename;
 
         const unity = {
@@ -76,7 +84,7 @@ module.exports = {
     async update(request, response) {
         const { id } = request.params;
         const { name, content, module_id } = request.body;
-        const image_url = `http://192.168.0.101:3333/uploads/${request.file.filename}`;
+        const image_url = `http://192.168.1.10:3333/uploads/${request.file.filename}`;
         const image_name = request.file.filename;
 
         const unity = await connection('unity').select('*').where('id', id).first();
